5442825341732aaa45ff4ab08b36cf792a1dd678
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2018-12-06 19:09:27 -1000

    Assignment complete

--- basics.js
+++ basics.js
@@ -1,24 +1,57 @@
 /* Create a `myName` variable and assign it a String value */
 
+var myName = 'Zeke';
+
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
+ var person = {
+        name: myName,
+        age: 19,
+
+ }
+
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+ var canDrive;
+ if ( person['age'] >= 16 ) {
+        canDrive = true;
+
+ } else {
+        canDrive = false;
+ }
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
 
+ function greet ( name ) {
+        console.log ( `Hello, my name is ${ name }` );
+
+ }
+
+ greet ( myName );
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
- */
+*/
+
+var dataTypes = [ 19, 'My Data', true, [ 1, 2, 3 ], { name: 'newPerson', age: 28, }, null, undefined, ];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+ var dog = {
+        bark: function ( ) {
+                console.log( 'WOOF!' );
+        },
+        name: 'Spot',
+
+ }
